42d05f508dcdfc94901dbb94d691eac198d12fbd
WuLiangMeiYue <31639782+WuLiangMeiYue@users.noreply.github.com>
2018-11-20 11:24:39 +0800

    init project

--- src/main/java/com/auto/create/file/document/DOCCreate.java
+++ src/main/java/com/auto/create/file/document/DOCCreate.java
@@ -1,50 +1,50 @@
 package com.auto.create.file.document;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DOCCreate {
 
-    private static String classPath = null;
-
     private static String basePath = null;
 
+    private static final File DOCUMENT = new File("c:\\users\\administrator\\desktop\\" + "接口文档.doc");
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DOCCreate.class);
+
     static {
-        classPath = DOCCreate.class.getPackage().toString();
-        classPath = classPath.substring(classPath.indexOf(' ') + 1);
-        basePath = classPath.replace('.', '\\') + "\\" + DOCCreate.class.getSimpleName();
+        try {
+            basePath = new File("").getAbsolutePath() + "\\src\\main\\java\\com\\auto\\create\\file\\document\\";
+            if (!DOCUMENT.exists()) {
+                DOCUMENT.createNewFile();
+            }
+            LOGGER.info("自动生成接口文档模块初始化完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         try {
-            File file = new File("c:\\users\\administrator\\desktop\\" + "接口文档.doc");
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
             File projectDirectory = new File(
                     "E:\\ClassManagement\\ClassManagementSystemProject\\src\\main\\java\\com\\school\\management\\api\\controller\\");
             for (File f : projectDirectory.listFiles()) {
-                Class content = readComment(f);
-//                System.out.println(content.getName());
+                readToClass(f.getName(), readComment(f));
+                break;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static Class readComment(File f) throws ClassNotFoundException {
+    private static String readComment(File f) throws ClassNotFoundException {
         String content = "";
-        File byteFile = new File("..\\ByteFile.java");
         try {
-            if (!byteFile.exists()) {
-                byteFile.createNewFile();
-            }
             if (f.canRead()) {
                 FileInputStream is = new FileInputStream(f);
-                FileOutputStream fileOutputStream = new FileOutputStream(byteFile);
                 byte[] b = new byte[is.available()];
                 int i = 0;
                 int index = 0;
@@ -53,15 +53,16 @@ public class DOCCreate {
                     index++;
                 }
                 content = new String(b);
-                fileOutputStream.write(b);
                 is.close();
-                fileOutputStream.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String name = byteFile.getName();
-        System.out.println(byteFile.getAbsolutePath());
-        return Class.forName(byteFile.getAbsolutePath());
+        return content;
     }
+
+    private static void readToClass(String className, String classPath) throws IOException, ClassNotFoundException {
+//        JavaStringCompiler
+    }
+
 }
